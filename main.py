b82eabed26fb727541427069517faa55af286904
robert <robert@auto-master.com>
2017-09-22 10:33:52 -0700

    This is now a pycharm project which should make life easier. I am still working on figuring out the database, but I am blocking out some basics

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
+from google.appengine.ext import db
+from google.appengine.api import users
 import webapp2
+
+
+class Stream(db.Model):
+    id = db.IntProperty()
+    author = db.UserProperty()
+    imgUrl = db.StringProperty()
+    createdDate = db.DateTimeProperty(auto_now_add=True)
+
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello, webapp2!')
 
+
+class Login(webapp2.RequestHandler):
+    def post(self, user_id):
+        #Need to return a session ID instead
+        self.response.write(user_id)
+
+
+class Management(webapp2.RequestHandler):
+    def get(self, user_id):
+        subscribed = db.GqlQuery("select a.* from streams a, subscriptions b "
+                                 "where b.author = :1 and a.id = b.streamId", user_id)
+        authored = db.GqlQuery("select * from streams "
+                               "where author = :1", user_id)
+        self.response.write(subscribed, authored)
+
+
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
+    (r'/login/(\w+)', Login)
 ], debug=True)
