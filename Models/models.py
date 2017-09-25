4c34d97548fc1fd3eb019f87c4ee0cb82308e47f
robert <robert@auto-master.com>
2017-09-24 19:50:48 -0700

    Fully working to create a Stream

--- Models/models.py
+++ Models/models.py
@@ -2,12 +2,14 @@ from google.appengine.ext import ndb
 
 
 class Stream(ndb.Model):
-    test = ndb.StringProperty()
-
+    imgUrl = ndb.StringProperty()
     createdDate = ndb.DateTimeProperty(auto_now_add=True)
     views = ndb.IntegerProperty()
     name = ndb.StringProperty()
     subscribers = ndb.IntegerProperty()
 
-    def __init__(self):
+    def __init__(self, name='Stream1', subscribers=8, image_url='image.jpg'):
         ndb.Model.__init__(self)
+        self.name = name
+        self.subscribers = subscribers
+        self.imageUrl = image_url
