60d618d41396f64075e63bc3bd6a3fde0dfe45d5
Emil <ebsaibes@gmail.com>
2017-09-26 20:03:29 -0500

    remove Service object.  each method in service is just a standalone function for now.

--- services/services.py
+++ services/services.py
@@ -2,30 +2,27 @@ from Models import models
 import json
 
 
-class Service:
-    def __init__(self):
-        # type: () -> None
-        pass
+@staticmethod
+def create_stream(name, subscribers, image_url):
+    """
 
-    @staticmethod
-    def create_stream(name, subscribers, image_url):
-        """
-        :param name:
-        :param subscribers:
-        :param image_url:
-        :return html:
-        """
+    :return:
+    :param name:
+    :param subscribers:
+    :param image_url:
+    :return html:
+    """
 
-        new_stream = models.Stream()
-        new_stream.name = name
-        new_stream.subscribers = subscribers
-        new_stream.imgUrl = image_url
-        new_stream.put()
-        return 200
+    new_stream = models.Stream()
+    new_stream.name = name
+    new_stream.subscribers = subscribers
+    new_stream.imgUrl = image_url
+    new_stream.put()
+    return 200
 
-    @staticmethod
-    def get_all_streams():
-        temp_stream = models.Stream
-        query = temp_stream.query().order(temp_stream.createdDate)
-        all_streams = query.fetch()
-        return all_streams
+
+def get_all_streams():
+    temp_stream = models.Stream
+    query = temp_stream.query().order(temp_stream.createdDate)
+    all_streams = query.fetch()
+    return all_streams
