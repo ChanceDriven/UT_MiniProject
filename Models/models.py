81ea4f340dcdd018d8598182152f9122625b5473
Emil <ebsaibes@gmail.com>
2017-09-26 23:36:17 -0500

    add image model

--- Models/models.py
+++ Models/models.py
@@ -2,14 +2,30 @@ from google.appengine.ext import ndb
 
 
 class Stream(ndb.Model):
-    imgUrl = ndb.StringProperty()
+    name = ndb.StringProperty()
+    coverImgUrl = ndb.StringProperty()
     createdDate = ndb.DateTimeProperty(auto_now_add=True)
     views = ndb.IntegerProperty()
-    name = ndb.StringProperty()
     subscribers = ndb.IntegerProperty()
 
-    def __init__(self, name='Stream1', subscribers=8, image_url='image.jpg'):
+    def __init__(self, name="Stream1", subscribers=8, image_url="image.jpg"):
         ndb.Model.__init__(self)
         self.name = name
         self.subscribers = subscribers
-        self.imageUrl = image_url
+        self.coverImgUrl = image_url
+
+
+class Image(ndb.Model):
+    streamId = ndb.StringProperty()
+    imgUrl = ndb.StringProperty()
+    updatedData = createdDate = ndb.DateTimeProperty(auto_now_add=True)
+
+    def __init__(self, stream_id, image_url="image.jpg"):
+        ndb.Model.__init__(self)
+        self.streamId = stream_id
+        self.image_url = image_url
+
+
+
+
+
